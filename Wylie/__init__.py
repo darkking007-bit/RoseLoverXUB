a6d79d1f70b83a92bc0b4684ab8e125d13f0aa74
darkking007-bit <80806602+darkking007-bit@users.noreply.github.com>
2021-10-01 12:06:54 +0530

    Update __init__.py

--- Wylie/__init__.py
+++ Wylie/__init__.py
@@ -13,6 +13,7 @@ StartTime = time.time()
 
 STRING_SESSION = os.environ.get("STRING_SESSION")
 STACY_SESSION = os.environ.get("STACY_SESSION")
+X_SESSION = os.environ.get("X_SESSION")
 TOKEN = os.environ.get("TOKEN")
 OWNER_ID = os.environ.get("OWNER_ID")
 API_KEY = os.environ.get("API_KEY")
@@ -21,4 +22,4 @@ API_HASH = os.environ.get("API_HASH")
 ubot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
 tbot = TelegramClient(None, API_KEY, API_HASH)
 stacy = TelegramClient(StringSession(STACY_SESSION), API_KEY, API_HASH)
-
+xbot = TelegramClient(StringSession(X_SESSION), API_KEY, API_HASH)
